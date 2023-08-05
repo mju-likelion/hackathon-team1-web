8a12dc5e72ab19af3ef4c52c3322f8932a5774ec
gyuhyuk <bruno3@naver.com>
2023-08-05 20:39:53 +0900

    feat: Header 보관함 이미지 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import logo from "../assets/images/Logo.svg";
+import locker from "../assets/images/Locker.svg";
 import Select from "./Select";
 
 const Header = ({ isVisible }) => {
@@ -7,6 +8,7 @@ const Header = ({ isVisible }) => {
   return (
     <Container>
       <Logo src={logo} alt="로고 이미지" $isVisible={isVisible} />
+      <Locker src={locker} alt="보관함 이미지" />
       <Select />
     </Container>
   );
@@ -17,7 +19,6 @@ export default Header;
 const Container = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
   width: 100%;
   height: 117px;
   padding: 0px 33px;
@@ -25,9 +26,20 @@ const Container = styled.div`
 `;
 
 const Logo = styled.img`
+  justify-self: start;
+  margin-right: auto;
   visibility: ${({ $isVisible }) => ($isVisible ? "visible" : "hidden")};
   width: 300px;
   height: 58px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Locker = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 30px;
   &:hover {
     cursor: pointer;
   }
