0d62e8e84dd548e0e7f02ab517e558d208ec944b
Jake-Kim38 <kjm25034841@gmail.com>
2023-08-08 20:43:36 +0900

    feat: 버튼 컴포넌트 추가 chore: 이미지 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+import Compare from "./pages/Compare";
 import GlobalStyle from "./styles/GlobalStyle";
+import { Theme } from "./styles/Theme";
+import { ThemeProvider } from "styled-components";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={Theme}>
       <GlobalStyle />
-    </>
+      <Compare />
+    </ThemeProvider>
   );
 }
 
