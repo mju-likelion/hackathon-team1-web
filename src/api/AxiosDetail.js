d64d310a6946b53563ac72a7e81e8b683d6132a1
gyuhyuk <bruno3@naver.com>
2023-08-14 22:23:40 +0900

    refactor: 코드 수정

--- src/api/AxiosDetail.js
+++ src/api/AxiosDetail.js
@@ -1,13 +1,13 @@
 import Axios from "./Axios";
 
 export const AxiosDetail = async (usecallbackFunction) => {
-  await Axios.get(
-    `/nestjs/api/insurance-suggesters/one/b37ff929-633c-450c-b19c-cfa2855609d9`
-  )
-    .then((res) => {
-      usecallbackFunction(res.data);
-    })
-    .catch((error) => {
-      error.response.data.message.map((message) => alert(message));
-    });
+  try {
+    const response = await Axios.get(
+      `/nestjs/api/insurance-suggesters/one/b37ff929-633c-450c-b19c-cfa2855609d9`
+    );
+    return usecallbackFunction(response.data);
+  } catch (error) {
+    console.log(error.response.data.detail);
+    throw error;
+  }
 };
