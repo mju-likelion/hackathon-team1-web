8e716037460ca8fe7837abac78048fc48d75175f
gyuhyuk <bruno3@naver.com>
2023-08-16 06:02:58 +0900

    feat: 언어별로 detail-page 렌더링 구현

--- src/components/DetailBox.jsx
+++ src/components/DetailBox.jsx
@@ -1,10 +1,15 @@
 import { styled } from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { LanguageAtom } from "../assets/atom/LanguageAtom";
 
 const DetailBox = ({ insuranceData }) => {
-  const { firstContent, secondContent, title } = insuranceData;
+  const { firstContent, secondContent, title, englishTitle, chineseTitle } =
+    insuranceData;
 
   const navigate = useNavigate();
+
+  const pageLanguage = useRecoilValue(LanguageAtom);
 
   if (!insuranceData) {
     navigate("/");
@@ -14,14 +19,28 @@ const DetailBox = ({ insuranceData }) => {
     <>
       <InsuranceDetailBox>
         <DetailTextBox>
-          <DetailTitle>{title}</DetailTitle>
+          <DetailTitle>
+            {pageLanguage === "KOR"
+              ? title
+              : pageLanguage === "ENG"
+              ? englishTitle
+              : chineseTitle}
+          </DetailTitle>
           {title === "가입연령" ? (
             <DetailContent>
-              {firstContent}~{secondContent}세
+              {pageLanguage === "KOR"
+                ? `${firstContent}~${secondContent}세`
+                : pageLanguage === "ENG"
+                ? `${firstContent}~${secondContent}years`
+                : `${firstContent}~${secondContent}岁`}
             </DetailContent>
           ) : (
             <DetailContent>
-              남 {firstContent}원{"\n"}여 {secondContent}원
+              {pageLanguage === "KOR"
+                ? `남 ${firstContent}원${"\n"}여 ${secondContent}원`
+                : pageLanguage === "ENG"
+                ? `Male ${firstContent}won${"\n"}Female ${secondContent}won`
+                : `男 ${firstContent}元${"\n"}女 ${secondContent}元`}
             </DetailContent>
           )}
         </DetailTextBox>
@@ -55,7 +74,7 @@ const DetailTitle = styled.div`
 `;
 
 const DetailContent = styled.div`
-  width: 141px;
+  width: 220px;
   height: 33px;
   font-size: 25px;
   line-height: 33px;
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -12,9 +12,13 @@ import Loading from "../components/Loading";
 import LargeButton from "../components/LargeButton";
 import { useParams } from "react-router-dom";
 import Modal from "../components/Modal";
+import { useRecoilState } from "recoil";
+import { LanguageAtom } from "../assets/atom/LanguageAtom";
 
 const Detail = () => {
   const { infoId } = useParams();
+
+  const pageLanguage = useRecoilState(LanguageAtom);
 
   const [count, setCount] = useState(0);
 
@@ -83,11 +87,15 @@ const Detail = () => {
   const insuranceArr = [
     {
       title: "가입연령",
+      englishTitle: "Entry Age",
+      chineseTitle: "投保年龄",
       firstContent: insurance.insuranceAgeGroupStart,
       secondContent: insurance.insuranceAgeGroupEnd,
     },
     {
       title: "보험료",
+      englishTitle: "Insurance Premium",
+      chineseTitle: "保险费",
       firstContent: insurance.premiumMale,
       secondContent: insurance.premiumFemale,
     },
@@ -97,35 +105,71 @@ const Detail = () => {
     {
       img: Protect,
       title: "예금자보호",
+      englishTitle: "Deposit Protection",
+      chineseTitle: "存款保护",
       content: `1인당 최고 ${insurance.depositorProtection}만원`,
+      englishContent: `One person up to ${insurance.depositorProtection}million won`,
+      chineseContent: `每人最高${insurance.depositorProtection}百万韩元`,
       explain: `이 상품은 예금자보호법에 따라 예금보험공사가 1인당 “최고 ${insurance.depositorProtection}만원” 한도로 보호합니다. (단, 법인 계약 제외)`,
+      englishExplain: `This product is protected by the Deposit Insurance Corporation with a maximum limit of ${insurance.depositorProtection} million won per person under the Deposit Protection Law. (Excluding corporate contracts)`,
+      chineseExplain: `这款产品受存款保险法保护, 每人最高限额为${insurance.depositorProtection}万韩元, 不包括法人合同.`,
     },
 
     {
       img: Guarantee,
       title: "보장성보험",
+      englishTitle: "Guaranteed Insurance",
+      chineseTitle: "保障保险",
       content: "상해, 질병 등",
+      englishContent: "Injuries, diseases, etc.",
+      chineseContent: "伤害、疾病等 (Shānghài, jíbìng děng)",
       explain:
         "이 상품은 만기시 환급금이 총 납입한 보험료를 초과하지 않습니다.",
+      englishExplain:
+        "This product does not exceed the total premiums paid in maturity refund.",
+      chineseExplain: "本产品不超过到期退还的总保费.",
     },
 
     {
       img: Calender,
       title: "청약철회기간",
+      englishTitle: "Withdrawal Period",
+      chineseTitle: "退保期限",
       content: `청약 후 ${insurance.cancellationPeriod}일 이내`,
+      englishContent: `Within ${insurance.cancellationPeriod} days after subscription`,
+      chineseContent: `在订阅后${insurance.cancellationPeriod}天内 (zài dìngyuè hòu ${insurance.cancellationPeriod} tiān nèi)`,
       explain: `이 상품은 청약을 한 날부터 ${
         insurance.cancellationPeriod
       }일 이내(보험증권을 받은 날부터 ${
         insurance.cancellationPeriod / 2
       }일 이내)에 청약철회가 가능합니다.`,
+      englishExplain: `
+        This product allows withdrawal of subscription within ${
+          insurance.cancellationPeriod
+        }days from the date of subscription (within ${
+        insurance.cancellationPeriod / 2
+      }days from the date of receiving the insurance policy).`,
+      chineseExplain: `该产品自申请购买之日起, 可以在${
+        insurance.cancellationPeriod
+      }天内（自收到保单之日起,${
+        insurance.cancellationPeriod / 2
+      }天内）申请撤销订购。`,
     },
 
     {
       img: Price,
       title: "보험가격지수",
+      englishTitle: "Insurance Price Index",
+      chineseTitle: "保险价格指数",
       content: `${insurance.priceIndex}% (낮을수록 저렴)`,
+      englishContent: `${insurance.priceIndex}% (Lower is cheaper)`,
+      chineseContent: `${insurance.priceIndex}% （越低越便宜)`,
       explain:
         "[보험가격지수] 보험가격지수 / 성별 / 표준형 계약 기준 산출식 = 영업보험료 ÷ (참조순보험료 + 상품군별 평균사업비) X 100 보험업계 평균가격대비 상품의 보험가격 수준(대표계약 기준)을 비교하는 지수로 보장내역이 동일한 경우 100% 초과이면 업계 평균보다 가격 수준이 높고, 100% 미만이면 업계 평균보다 가격수준이 낮습니다. * 다만, 보험가격지수는 업계평균 영업보험료 대비 동상품의 영업보험료 수준을 나타내는 지표로써, 회사별로 사업비가 상이하므로 보험가격지수가 낮다고 하여 반드시 보험료가 저렴한 것은 아닙니다.",
+      englishExplain:
+        "[Insurance Price Index] The insurance price index is calculated based on the formula: Insurance Price Index / Gender / Standard Contract Basis = Sales Insurance Premium / (Reference Net Insurance Premium + Average Business Expenses by Product Category) X 100. This index compares the price level of insurance products to the industry average price. If the index is above 100%, the price level is higher than the industry average, and if it's below 100%, the price level is lower than the industry average. However, please note that the insurance price index is an indicator of the level of insurance premium compared to the industry average sales insurance premium, and it doesn't necessarily mean that lower index values always indicate lower premiums, as business expenses can vary between companies.",
+      chineseExplain:
+        "【保险价格指数】保险价格指数根据以下公式计算：保险价格指数 / 性别 / 标准合同基础 = 销售保险费 /（参考净保费 + 按产品类别平均业务费用）X 100。该指数将保险产品的价格水平与行业平均价格进行比较。如果指数超过100％,则价格水平高于行业平均水平；如果低于100％,则价格水平低于行业平均水平。但是,请注意,保险价格指数是将保险费用与行业平均销售保险费用进行比较的指标,并不意味着较低的指数值总是表示较低的保费,因为公司的业务费用可能会有所不同.",
     },
   ];
 
@@ -156,7 +200,11 @@ const Detail = () => {
             )}
             <ImageText onClick={addCompare}>
               <CompareImage src={Box} alt="비교함 담기 이미지" />
-              비교함 담기
+              {pageLanguage[0] === "KOR"
+                ? "비교함 담기"
+                : pageLanguage[0] === "ENG"
+                ? "add to compare"
+                : "加入比较箱"}
             </ImageText>
           </LeftArea>
           <RightArea>
@@ -180,7 +228,13 @@ const Detail = () => {
           </RightArea>
         </AboveContainer>
         <BelowContainer>
-          <MainText>주요정보</MainText>
+          <MainText>
+            {pageLanguage[0] === "KOR"
+              ? "주요정보"
+              : pageLanguage[0] === "ENG"
+              ? "Key Information"
+              : "重要信息"}
+          </MainText>
           <InformationContainer>
             {mainArr.map((item, index) => (
               <InformationBox MainData={item} key={index} />
