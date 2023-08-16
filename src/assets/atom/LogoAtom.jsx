9d1a404b5b094aa5187eb479c9d29085fc58cb68
gyuhyuk <bruno3@naver.com>
2023-08-17 05:54:56 +0900

    feat: recoil 새로고침 시 상태 유지

--- src/assets/atom/LogoAtom.jsx
+++ src/assets/atom/LogoAtom.jsx
@@ -1,6 +1,10 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const LogoAtom = atom({
   key: "LogoAtom",
   default: false,
+  effects_UNSTABLE: [persistAtom],
 });
